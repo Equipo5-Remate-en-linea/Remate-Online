0bcadcf2ad5aadc4bd9b712bafb07eff01f4f154
Diego Alonso Morales Arellano <diego.moralesar@sansano.usm.cl>
2024-09-23 15:51:23 -0300

    Codigo base

---
+++ models/Producto.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const productoSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  precioInicial: { type: Number, required: true },
+  descripcion: { type: String, required: true },
+  imagen: { type: String, required: true },
+  duracion: { type: Number, required: true }, // Duración en minutos
+  ofertas: [{ type: Number }], // Array para las ofertas
+  categoria: { type: String, required: true } 
+});
+
+const Producto = mongoose.model('Producto', productoSchema);
+module.exports = Producto;
---
+++ remates-frontend/src/App.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import './Productos.css'; // Importar el archivo de estilos CSS
+
+function Productos() {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    // Hacer la solicitud al backend para obtener los productos
+    fetch('http://localhost:5000/productos')
+      .then(response => response.json())
+      .then(data => setProductos(data))
+      .catch(error => console.error('Error al obtener los productos:', error));
+  }, []);
+
+  return (
+    <div className="productos-container">
+      <h1>Productos en Remate</h1>
+      <div className="productos-grid">
+        {productos.length > 0 ? (
+          productos.map(producto => (
+            <div className="producto-card" key={producto._id}>
+              <div className="producto-imagen">
+                {producto.imagen ? (
+                  <img src={producto.imagen} alt={producto.nombre} />
+                ) : (
+                  <div className="placeholder-imagen">Imagen no disponible</div>
+                )}
+              </div>
+              <h2>{producto.nombre}</h2>
+              <p>{producto.descripcion}</p>
+              <p>Precio inicial: ${producto.precioInicial}</p>
+            </div>
+          ))
+        ) : (
+          <p>No hay productos disponibles</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Productos;
