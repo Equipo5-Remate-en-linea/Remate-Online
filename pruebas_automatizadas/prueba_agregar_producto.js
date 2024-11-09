6fb91461e9e885b4643ed9e8d41a6a3dd092aa86
seed4407 <124889238+seed4407@users.noreply.github.com>
2024-11-09 16:32:55 -0300

    Update prueba_agregar_producto.js

--- pruebas_automatizadas/prueba_agregar_producto.js
+++ pruebas_automatizadas/prueba_agregar_producto.js
@@ -30,37 +30,37 @@ const puppeteer = require('puppeteer');
 
   await page.click('#SubmitInicioSesion'); 
 
-  await page.waitForSelector('#Modal_inicio_sesion', { hidden: true, timeout: 5000  });
+  await page.waitForSelector('#Modal_inicio_sesion', { hidden: true, timeout: 100000  });
 
   try{
-    await page.waitForSelector('#Administrar_productos',  { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#Administrar_productos',  { visible: true, timeout: 100000 }); 
     await page.click('#Administrar_productos'); 
   
-    await page.waitForSelector('#Agregar_producto',{ visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#Agregar_producto',{ visible: true, timeout: 100000 }); 
     await page.click('#Agregar_producto');
     
-    await page.waitForSelector('#Formulario_agregar',{ visible: true, timeout: 5000 });
+    await page.waitForSelector('#Formulario_agregar',{ visible: true, timeout: 100000 });
   
-    await page.waitForSelector('#nombre_producto', { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#nombre_producto', { visible: true, timeout: 100000 }); 
     await page.type('#nombre_producto', 'producto de prueba');
     
-    await page.waitForSelector('#descripcion_producto', { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#descripcion_producto', { visible: true, timeout: 100000 }); 
     await page.type('#descripcion_producto', 'descripcion de prueba');
     
     const filePath = 'caja.jpg'; 
     const inputUploadHandle = await page.$('#imagen_producto'); 
     await inputUploadHandle.uploadFile(filePath);
   
-    await page.waitForSelector('#precioInicial', { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#precioInicial', { visible: true, timeout: 100000 }); 
     await page.type('#precioInicial', '1000');
   
-    await page.waitForSelector('#duracion_remate', { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#duracion_remate', { visible: true, timeout: 100000 }); 
     await page.type('#duracion_remate', '2');
   
-    await page.waitForSelector('#categoria_producto', { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#categoria_producto', { visible: true, timeout: 100000 }); 
     await page.select('#categoria_producto', 'tecnologia');
   
-    await page.waitForSelector('#submitAgregarProducto', { visible: true, timeout: 5000 }); 
+    await page.waitForSelector('#submitAgregarProducto', { visible: true, timeout: 100000 }); 
     await page.click('#submitAgregarProducto');  
     resultMessage1 = 'Producto agregado exitosamente'
 
