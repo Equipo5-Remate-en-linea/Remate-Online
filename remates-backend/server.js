ca2307fc44e127f1457d7cd79c68e7700b300861
seed4407 <124889238+seed4407@users.noreply.github.com>
2024-11-09 17:41:45 -0300

    Update server.js

--- remates-backend/server.js
+++ remates-backend/server.js
@@ -141,7 +141,7 @@ cron.schedule('* * * * *', async () => {
 });
 
 // Puerto
-const PORT = (hostname === 'localhost' ? 6000 : 6000);
+const PORT = (hostname === 'localhost' ? 5000 : 5000);
 app.listen(PORT,'0.0.0.0',() => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
